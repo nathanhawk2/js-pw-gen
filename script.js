d8877c5e4732097817ea3a375c5ef72fcb7546e4
Nathan <ntrumpet620@gmail.com>
2022-08-19 11:03:22 -0400

    finished all prompts, set up for pw gen and string

--- script.js
+++ script.js
@@ -2,30 +2,44 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
+  // password parameters
   var lower = 'abcdefghijklmnopqrstuvwxyz';
   var possible = '';
   var upper = lower.toUpperCase;
-  var numbers = '0123456789'
+  var numbers = '0123456789';
   var special = '"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
   var password = '';
 
-  var characters = 12; // need to get from prompt
+// character length prompt
+  var charLength = prompt ('Choose a length between 8-128 characters');
+  if (charLength > 129 || charLength < 8 || typeof charLength !== 'number') {
+    window.alert('Please input a numeric value between 8-128')
+    var charLength = prompt ('Choose a length between 8-128 characters')
+  };
 
+  // pw parameter prompts
   var hasLower = confirm('Do you want to include lowercase letters?');
   var hasUpper = confirm('Do you want to include uppercase letters?');
   var hasSpecial = confirm('Do you want to include special characters?');
   var hasNumbers = confirm('Do you want to include numbers?');
 
+if (!hasSpecial && !hasUpper && !hasLower && !hasNumbers) {
+  window.alert ('You must pick at least one parameter')
+  return;
+};
+
+// string building 
   if (hasLower) {
     possible += lower;
   }
 
+  // password generator
   for (var i = 0; i < characters; i++) {
     // find random letter from possible
     // append to the password string
   }
 
-  debugger;
+
 
   return '';
 }
